f2f061e59bd353d0bd56c4b7649e6d50d6cd73c4
Ak740s <pablofdezlpz09@gmail.com>
2025-03-26 22:29:11 +0100

    gui(hotfix): Fixed error in logic

--- src/gui/guidear.py
+++ src/gui/guidear.py
@@ -11,7 +11,7 @@ class XenoVagrantGUI:
         except:
             print("Could not set DPI awareness. Check help or about and contact @akhos09")
         
-        self.icon_path = "test.ico"
+        self.icon_path = "./assets/test.ico"
         if not os.path.exists(self.icon_path):
             print(f"""
                   Icon file not found: {self.icon_path}
@@ -22,31 +22,29 @@ class XenoVagrantGUI:
     def theme_callback(self, app_data, user_data):
         dark_theme = create_theme_imgui_dark()
         light_theme = create_theme_imgui_light()
-        if user_data == "Light Mode":
+        if user_data == "Light Theme":
             dpg.bind_theme(light_theme)
-        elif user_data=="Black Mode":
+        elif user_data=="Dark Theme":
             dpg.bind_theme(dark_theme)
         else:
+            dpg.bind_theme(None)
             pass    
 
     def menu(self):
         dpg.create_context()
-        dpg.create_viewport(title="XenoVagrant", width=1280, height=720)
+        dpg.create_viewport(title="XenoVagrant", width=1280, height=720,small_icon=self.icon_path,large_icon=self.icon_path)
         
-        dpg.set_viewport_small_icon(self.icon_path)
-        dpg.set_viewport_large_icon(self.icon_path)
-        
-        with dpg.window(tag="App"):
+        with dpg.window(tag="main_window"):
             dpg.add_combo(
             label="Theme Selector",
-            items=["Black Mode", "Light Mode"], 
-            default_value="Black Mode",
+            items=["Default Theme","Dark Theme", "Light Theme"], 
             callback=self.theme_callback,
+            default_value="Default Theme",
             width=200,
             tag="theme_selector"
         )
         
-        dpg.set_primary_window("App", True)
+        dpg.set_primary_window("main_window", True)
         
         dpg.setup_dearpygui()
         dpg.show_viewport()
